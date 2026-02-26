c243d6b64ad68abe7582b2f8e57d8757bbc8d40d
Sharon V <sharonarmin84@gmail.com>
2026-02-26 23:10:01 +0530

    project

--- project-slider.js
+++ project-slider.js
@@ -1,9 +1,9 @@
 const swiper = new Swiper('.project-swiper', {
   loop: true,
-  speed: 900,
+  speed: 800,
   effect: 'fade',
   fadeEffect: { crossFade: false },
-  autoplay: { delay: 4500, disableOnInteraction: false },
+  autoplay: { delay: 1000, disableOnInteraction: false },
   pagination: {
     el: '.project-pagination',
     clickable: true,
